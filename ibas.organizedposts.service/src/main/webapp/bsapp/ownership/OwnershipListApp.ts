776abd90667c86bbd57b72cca0fd3bf8829d2d8e
niurenzhu <niuren.zhu@icloud.com>
2018-01-20 16:28:59 +0800

    提示选择删除的数据

--- ibas.organizedposts.service/src/main/webapp/bsapp/ownership/OwnershipListApp.ts
+++ ibas.organizedposts.service/src/main/webapp/bsapp/ownership/OwnershipListApp.ts
@@ -132,6 +132,9 @@ export class OwnershipListApp extends ibas.BOListApplication<IOwnershipListView,
         }
         // 没有选择删除的对象
         if (beDeleteds.length === 0) {
+            this.messages(ibas.emMessageType.WARNING, ibas.i18n.prop("shell_please_chooose_data",
+                ibas.i18n.prop("shell_data_delete")
+            ));
             return;
         }
         let that: this = this;
