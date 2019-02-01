c0b44768d08467ef835836564d4c77faf4fa2280
牛加人等于朱 <niuren.zhu@icloud.com>
2019-02-01 16:25:45 +0800

    优化代码

--- ibas.organizedposts.service/src/main/webapp/bsapp/position/PositionListApp.ts
+++ ibas.organizedposts.service/src/main/webapp/bsapp/position/PositionListApp.ts
@@ -102,23 +102,7 @@ namespace organizedposts {
             }
             /** 删除数据，参数：目标数据集合 */
             protected deleteData(data: bo.Position | bo.Position[]): void {
-                // 检查目标数据
-                if (ibas.objects.isNull(data)) {
-                    this.messages(ibas.emMessageType.WARNING, ibas.i18n.prop("shell_please_chooose_data",
-                        ibas.i18n.prop("shell_data_delete")
-                    ));
-                    return;
-                }
-                let beDeleteds: ibas.ArrayList<bo.Position> = new ibas.ArrayList<bo.Position>();
-                if (data instanceof Array) {
-                    for (let item of data) {
-                        item.delete();
-                        beDeleteds.add(item);
-                    }
-                } else {
-                    data.delete();
-                    beDeleteds.add(data);
-                }
+                let beDeleteds: ibas.IList<bo.Position> = ibas.arrays.create(data);
                 // 没有选择删除的对象
                 if (beDeleteds.length === 0) {
                     this.messages(ibas.emMessageType.WARNING, ibas.i18n.prop("shell_please_chooose_data",
@@ -126,6 +110,10 @@ namespace organizedposts {
                     ));
                     return;
                 }
+                // 标记删除对象
+                beDeleteds.forEach((value) => {
+                    value.delete();
+                });
                 let that: this = this;
                 this.messages({
                     type: ibas.emMessageType.QUESTION,
@@ -133,44 +121,34 @@ namespace organizedposts {
                     message: ibas.i18n.prop("shell_multiple_data_delete_continue", beDeleteds.length),
                     actions: [ibas.emMessageAction.YES, ibas.emMessageAction.NO],
                     onCompleted(action: ibas.emMessageAction): void {
-                        if (action === ibas.emMessageAction.YES) {
-                            try {
-                                let boRepository: bo.BORepositoryOrganizedPosts = new bo.BORepositoryOrganizedPosts();
-                                let saveMethod: Function = function (beSaved: bo.Position): void {
-                                    boRepository.savePosition({
-                                        beSaved: beSaved,
-                                        onCompleted(opRslt: ibas.IOperationResult<bo.Position>): void {
-                                            try {
-                                                if (opRslt.resultCode !== 0) {
-                                                    throw new Error(opRslt.message);
-                                                }
-                                                // 保存下一个数据
-                                                let index: number = beDeleteds.indexOf(beSaved) + 1;
-                                                if (index > 0 && index < beDeleteds.length) {
-                                                    saveMethod(beDeleteds[index]);
-                                                } else {
-                                                    // 处理完成
-                                                    that.busy(false);
-                                                    that.messages(ibas.emMessageType.SUCCESS,
-                                                        ibas.i18n.prop("shell_data_delete") + ibas.i18n.prop("shell_sucessful"));
-                                                }
-                                            } catch (error) {
-                                                that.busy(false);
-                                                that.messages(ibas.emMessageType.ERROR,
-                                                    ibas.i18n.prop("shell_data_delete_error", beSaved, error.message));
-                                            }
-                                        }
-                                    });
-                                    that.proceeding(ibas.emMessageType.INFORMATION, ibas.i18n.prop("shell_data_deleting", beSaved));
-                                };
-                                that.busy(true);
-                                // 开始保存
-                                saveMethod(beDeleteds.firstOrDefault());
-                            } catch (error) {
-                                that.busy(false);
-                                that.messages(error);
-                            }
+                        if (action !== ibas.emMessageAction.YES) {
+                            return;
                         }
+                        let boRepository: bo.BORepositoryOrganizedPosts = new bo.BORepositoryOrganizedPosts();
+                        ibas.queues.execute(beDeleteds, (data, next) => {
+                            // 处理数据
+                            boRepository.savePosition({
+                                beSaved: data,
+                                onCompleted(opRslt: ibas.IOperationResult<bo.Position>): void {
+                                    if (opRslt.resultCode !== 0) {
+                                        next(new Error(ibas.i18n.prop("shell_data_delete_error", data, opRslt.message)));
+                                    } else {
+                                        next();
+                                    }
+                                }
+                            });
+                            that.proceeding(ibas.emMessageType.INFORMATION, ibas.i18n.prop("shell_data_deleting", data));
+                        }, (error) => {
+                            // 处理完成
+                            if (error instanceof Error) {
+                                that.messages(ibas.emMessageType.ERROR, error.message);
+                            } else {
+                                that.messages(ibas.emMessageType.SUCCESS,
+                                    ibas.i18n.prop("shell_data_delete") + ibas.i18n.prop("shell_sucessful"));
+                            }
+                            that.busy(false);
+                        });
+                        that.busy(true);
                     }
                 });
             }
